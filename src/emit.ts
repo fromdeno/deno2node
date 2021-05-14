e7fda827462d1bf753b2f6ad76ee321931172540
Wojciech Pawlik <woj.pawlik@gmail.com>
2021-05-14 12:12:35 +0200

    Simplify `transpileShebang`

--- src/emit.ts
+++ src/emit.ts
@@ -1,25 +1,35 @@
 import type { Diagnostic, MemoryEmitResultFile, Project } from "./deps.deno.ts";
 
+const anyShebang = /^#![^\n]*\n/;
+const denoShebang = /^#!\/usr\/bin\/env -S deno run\b[^\n]*\n/;
+const nodeShebang = "#!/usr/bin/env node\n";
+
 function transpileShebang(file: MemoryEmitResultFile) {
-  const regex = /^#!\/usr\/bin\/env -S deno run\b[^\n]*/;
-  const match = regex.exec(file.text);
-  if (match === null) return false;
-  const isJs = file.filePath.endsWith(".js");
-  const newShebang = isJs ? "#!/usr/bin/env node" : "";
-  file.text = newShebang + file.text.slice(match[0].length);
-  return isJs;
+  file.text = file.filePath.endsWith(".js")
+    ? file.text.replace(denoShebang, nodeShebang)
+    : file.text.replace(anyShebang, "\n");
 }
 
-async function markExecutable({ filePath }: MemoryEmitResultFile) {
+async function markExecutableIfNeeded(file: MemoryEmitResultFile) {
   if (Deno.build.os === "windows") return;
-  await Deno.chmod(filePath, 0o755);
+  if (!file.text.startsWith(nodeShebang)) return;
+  await Deno.chmod(file.filePath, 0o755);
 }
 
+/**
+ * Emits project to the filesystem.
+ * Returns diagnostics.
+ *
+ * Replaces Deno shebang with Node.js shebang in JS outputs.
+ * Removes shebangs from non-JS outputs.
+ * Then `chmod +x`'s outputs with Node.js shebang.
+ */
 export async function emit(project: Project): Promise<Diagnostic[]> {
   const result = project.emitToMemory();
-  const executables = result.getFiles().filter(transpileShebang);
+  const files = result.getFiles();
+  files.forEach(transpileShebang);
   await result.saveFiles();
-  await Promise.all(executables.map(markExecutable));
+  await Promise.all(files.map(markExecutableIfNeeded));
   const preEmitDiagnostics = project.getPreEmitDiagnostics();
   if (preEmitDiagnostics.length !== 0) return preEmitDiagnostics;
   return result.getDiagnostics();
