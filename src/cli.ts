223a0bea73a38e64d27982b479ac5b11736342a7
KnorpelSenf <shtrog@gmail.com>
2021-08-25 08:43:58 +0200

    Refactor CLI structure

--- src/cli.ts
+++ src/cli.ts
@@ -2,17 +2,23 @@
 import { ts } from "./deps.deno.ts";
 import { Context, deno2node, emit } from "./mod.ts";
 
-if (Deno.args.length !== 1 || Deno.args[0].startsWith("-")) {
-  console.error("Usage: deno2node <tsConfigFilePath>");
-  Deno.exit(2);
+async function cli(tsConfigFilePath?: string) {
+  const ctx = new Context({ tsConfigFilePath });
+  await deno2node(ctx);
+  const diagnostics = await emit(ctx.project);
+  if (diagnostics.length !== 0) {
+    console.info(ctx.project.formatDiagnosticsWithColorAndContext(diagnostics));
+    console.info("TypeScript", ts.version);
+    console.info(`Found ${diagnostics.length} errors.`);
+    Deno.exit(1);
+  }
 }
 
-const ctx = new Context({ tsConfigFilePath: Deno.args[0] });
-await deno2node(ctx);
-const diagnostics = await emit(ctx.project);
-if (diagnostics.length !== 0) {
-  console.info(ctx.project.formatDiagnosticsWithColorAndContext(diagnostics));
-  console.info("TypeScript", ts.version);
-  console.info(`Found ${diagnostics.length} errors.`);
-  Deno.exit(1);
+if (import.meta.main) {
+  if (Deno.args.length !== 1 || Deno.args[0].startsWith("-")) {
+    console.error("Usage: deno2node <tsConfigFilePath>");
+    Deno.exit(2);
+  }
+  const tsConfigFilePath = Deno.args[0];
+  await cli(tsConfigFilePath);
 }
