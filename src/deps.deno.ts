588d788676e6ad7ad3ae5c2a94e62d76d1fce179
KnorpelSenf <shtrog@gmail.com>
2021-11-21 17:46:18 +0100

    Update ts-morph to 13.0.0 and update other deps too

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,4 +1,4 @@
 // please keep sorted
-export { z } from "https://cdn.skypack.dev/zod@v3.8.2?dts";
-export * as path from "https://deno.land/std@0.106.0/path/mod.ts";
-export * from "https://deno.land/x/ts_morph@12.0.0/mod.ts";
+export { z } from "https://cdn.skypack.dev/zod@v3.11.6?dts";
+export * as path from "https://deno.land/std@0.115.0/path/mod.ts";
+export * from "https://deno.land/x/ts_morph@13.0.0/mod.ts";
