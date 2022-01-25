515300483ed8291b8cfb5a98ee3778f755840771
KnorpelSenf <shtrog@gmail.com>
2022-01-25 23:35:10 +0100

    Add deno.ns shim

--- src/shim.node.ts
+++ src/shim.node.ts
@@ -1,19 +1,7 @@
 // Node-only, see https://github.com/fromdeno/deno2node#shimming
-import { chmod, mkdir, readFile, writeFile } from "fs/promises";
-import { URL } from "url";
-export { URL };
-const os = process.platform === "win32" ? "windows" : process.platform;
-export const Deno = {
-  // please keep sorted
-  args: process.argv.slice(2),
-  build: { os },
-  chmod,
-  cwd: process.cwd,
-  env: { get: (key: string) => process.env[key] },
-  exit: process.exit,
-  mkdir,
-  writeTextFile: writeFile,
-};
+import { readFile } from "fs/promises";
+
+export * from "deno.ns";
 
 export async function fetch(fileUrl: URL) {
   if (fileUrl.protocol !== "file:") {
