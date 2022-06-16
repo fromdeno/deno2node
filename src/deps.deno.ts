5f8381fbd471b4874d261a330c4ff6bbe8581d8d
Wojciech Pawlik <woj.pawlik@gmail.com>
2022-06-16 14:58:26 +0200

    Upgrade to TypeScript 4.7

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,5 +1,5 @@
 // Deno-only, see https://doc.deno.land/https/deno.land/x/deno2node/src/mod.ts#deno2node
 // please keep sorted
 export { z } from "https://cdn.skypack.dev/zod@v3.11.6?dts";
-export * as path from "https://deno.land/std@0.128.0/path/mod.ts";
-export * from "https://deno.land/x/ts_morph@14.0.0/mod.ts";
+export * as path from "https://deno.land/std@0.140.0/path/mod.ts";
+export * from "https://deno.land/x/ts_morph@15.1.0/mod.ts";
