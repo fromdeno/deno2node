f12469303d1f3fe3e63a115e510c118a7356241b
Wojciech Pawlik <woj.pawlik@gmail.com>
2022-09-10 05:25:43 +0200

    Simplify `--help`

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,8 @@
-#!/usr/bin/env -S deno run --allow-read --allow-write --allow-env=NODE_DEBUG
+#!/usr/bin/env -S deno run --no-prompt --allow-read=. --allow-write=.
 import { ts } from "./deps.deno.ts";
 import { getHelpText } from "./help.ts";
 import { getVersion, initializeProject } from "./init.ts";
 import { Context, deno2node, emit } from "./mod.ts";
-
-const HELP_ARGS = new Set(["-h", "--help", "-help", "-?"]);
-
-if (Deno.args.some((arg) => HELP_ARGS.has(arg.trim()))) {
-  console.log(getHelpText(await getVersion()));
-  Deno.exit(0);
-}
 
 const { options, fileNames, errors } = ts.parseCommandLine(Deno.args);
 const tsConfigFilePath = options.project ?? fileNames[0] ?? "tsconfig.json";
@@ -19,6 +12,11 @@ if (errors.length) {
     console.error(error.messageText);
   }
   Deno.exit(2);
+}
+
+if (options.help) {
+  console.log(getHelpText(await getVersion()));
+  Deno.exit(0);
 }
 
 if (options.version) {
