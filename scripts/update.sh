518d10325c9e16268f45a6d447bfaa51cf7621c4
Wojciech Pawlik <woj.pawlik@gmail.com>
2022-09-29 13:08:18 +0200

    Upgrade to TypeScript 4.8

---
+++ scripts/update.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eu
+
+export PATH="$PWD/node_modules/.bin:$PATH"
+
+cp package.json package.json~
+
+oldTsVersion="$(deno eval --print 'Deno.version.typescript.match(/^\d+\.\d+/, "")[0]')"
+npm install --save-dev --save-prefix='~' 'deno-bin@*'
+newTsVersion="$(deno eval --print 'Deno.version.typescript.match(/^\d+\.\d+/, "")[0]')"
+
+if [ "$oldTsVersion" == "$newTsVersion" ]; then
+  echo 'Already up to date.'
+  mv package.json~ package.json
+  exit
+fi
+
+npm install-test 'ts-morph@*'
+git add src/deps.deno.ts
+
+npm version minor --force --message "Upgrade to TypeScript ${newTsVersion}"
+
+rm package.json~
