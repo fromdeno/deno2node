aa98f1beb4d32932eb6d12338550a83c1df6b71f
Wojciech Pawlik <woj.pawlik@gmail.com>
2022-11-24 16:28:36 +0100

    Generalize specifier parsing; fixes #35

--- src/_transformations/specifiers.ts
+++ src/_transformations/specifiers.ts
@@ -1,17 +1,16 @@
 import XR from "https://cdn.skypack.dev/xregexp@5.1.0?dts";
 
-// from https://github.com/npm/validate-npm-package-name/blob/80313b82a5c7e466556c3a7fb1cb7d7dab92f4be/index.js#L3
-const scopedPackage = new RegExp("^(?:@([^/]+?)[/])?([^/]+?)$");
-// from https://semver.org/
-const version = XR.tag("n")
-  `^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`;
+const scopedPackage = /^(?:@[\w.-]+\/)?[\w.-]+$/;
+const version = /^[^/?]+$/;
 
 const services = [
-  XR.tag("x")`^npm:	(${scopedPackage})@${version}`,
-  XR.tag("x")`^https://cdn\.skypack\.dev/	(${scopedPackage})@${version}\?`,
-  XR.tag("x")`^https://esm\.sh/	(${scopedPackage})@${version}$`,
-  XR.tag("x")`^https://deno\.land/	std(?:@${version})?/node/(\w+)\.ts$`,
-  XR.tag("x")`^https://nest\.land/	std/node/${version}/(\w+)\.ts$`,
+  XR.tag("x")`^npm:	(${scopedPackage})(?:@${version})?$`,
+  XR.tag(
+    "x",
+  )`^https://cdn\.skypack\.dev/	(${scopedPackage})(?:@${version})?\?`,
+  XR.tag("x")`^https://esm\.sh/	(${scopedPackage})(?:@${version})?$`,
+  XR.tag("x")`^https://deno\.land/	std(?:@${version})?/node/([\w/]+)\.ts$`,
+  XR.tag("x")`^https://nest\.land/	std/node/${version}/([\w/]+)\.ts$`,
 ];
 
 const transpileHttpsImport = (specifier: string) => {
