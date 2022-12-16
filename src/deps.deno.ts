25b4c37d9e78d625630c88c08a957ae50c383ca4
Wojciech Pawlik <woj.pawlik@gmail.com>
2022-12-16 11:48:53 +0100

    Upgrade to TypeScript 4.9

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,3 +1,3 @@
 // Deno-only, see https://doc.deno.land/https/deno.land/x/deno2node/src/mod.ts#deno2node
 // Auto-updated in `pretest` script
-export * from "https://deno.land/x/ts_morph@16.0.0/mod.ts";
+export * from "https://deno.land/x/ts_morph@17.0.1/mod.ts";
