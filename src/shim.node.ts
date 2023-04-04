3e47aaf5855a1f4df47f1255f64ab112068884a9
Wojciech Pawlik <woj.pawlik@gmail.com>
2023-04-04 12:53:13 +0200

    Ditch `@deno/shim-deno`
    https://github.com/denoland/node_shims/pull/134

--- src/shim.node.ts
+++ src/shim.node.ts
@@ -1,12 +1,22 @@
 // Node-only, see https://github.com/fromdeno/deno2node#shimming
-import { readFile } from "fs/promises";
+import { chmod, readFile } from "node:fs/promises";
+import process from "node:process";
+import { isatty } from "node:tty";
 
-export * from "@deno/shim-deno";
+const os = process.platform === "win32" ? "windows" : process.platform;
+
+export const Deno = {
+  // please keep sorted
+  args: process.argv.slice(2),
+  build: { os },
+  chmod,
+  cwd: process.cwd,
+  exit: process.exit,
+  isatty,
+  stdout: { rid: process.stdout.fd },
+};
 
 export async function fetch(fileUrl: URL) {
-  if (fileUrl.protocol !== "file:") {
-    throw new Error("Can only read local files!");
-  }
   const data = await readFile(fileUrl, { encoding: "utf-8" });
   return {
     json: () => JSON.parse(data),
