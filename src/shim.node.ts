938422fd0502a5cdbec167e54b0a30a24c4bab0c
Wojciech Pawlik <woj.pawlik@gmail.com>
2023-04-04 23:14:01 +0200

    Avoid `Deno` namespace

--- src/shim.node.ts
+++ src/shim.node.ts
@@ -1,15 +1,11 @@
 // Node-only, see https://github.com/fromdeno/deno2node#shimming
-import { chmod, readFile } from "node:fs/promises";
-import process from "node:process";
+import { readFile } from "node:fs/promises";
+import * as process from "node:process";
 import { isatty } from "node:tty";
-
-const os = process.platform === "win32" ? "windows" : process.platform;
 
 export const Deno = {
   // please keep sorted
   args: process.argv.slice(2),
-  build: { os },
-  chmod,
   exit: process.exit,
   isatty,
   noColor: process.env.NO_COLOR !== undefined,
