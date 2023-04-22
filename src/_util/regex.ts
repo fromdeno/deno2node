e6eb4d8206fdd6dbf34f3eab18271f0720345076
Wojciech Pawlik <woj.pawlik@gmail.com>
2023-04-22 19:47:01 +0200

    Add provenance
    https://docs.npmjs.com/generating-provenance-statements

--- src/_util/regex.ts
+++ src/_util/regex.ts
@@ -1,4 +1,20 @@
+export type RegExpSource = Pick<RegExp, "source">;
+
+const _source = (arg: string | RegExpSource) =>
+  typeof arg === "string"
+    ? arg.replace(/[.*+?^${}()|[\]\\]/g, "\\$&") // escape string
+    : arg.source;
+
 export const tag = (
   literals: TemplateStringsArray,
-  ...substitutions: RegExp[]
-) => new RegExp(String.raw(literals, ...substitutions.map((re) => re.source)));
+  ...substitutions: ReadonlyArray<string | RegExpSource>
+) => {
+  const subpatterns = substitutions.map((sub) => `(?:${_source(sub)})`);
+  return new RegExp(String.raw(literals, ...subpatterns));
+};
+
+export const union = (
+  strings: ReadonlyArray<string | RegExpSource>,
+): RegExpSource => ({
+  source: strings.map(_source).join("|"),
+});
